3a4440f1dfaad4a71a13f0397915e176e097e58c
LiLQD <luuquangdung1511@gmail.com>
2025-09-16 02:58:34 +0700

    Add playRound function to check who is winner and add 1 point

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let humanScore = 0 
+let humanScore = 0
 let computerScore = 0
 
 /* 
@@ -8,17 +8,17 @@ Use condition statement to check:
 if var <= 33 return rock
 else if var >33 <=66 return paper
 else return scissors 
-*/ 
-function getComputerChoice(){
+*/
+function getComputerChoice() {
     let randomNumber = Math.round(Math.random() * 100)
     console.log(randomNumber)
-    if(randomNumber <= 33){
+    if (randomNumber <= 33) {
         return "Rock"
     }
-    else if(randomNumber > 33 && randomNumber <= 66){
+    else if (randomNumber > 33 && randomNumber <= 66) {
         return "Paper"
     }
-    else{
+    else {
         return "Scissors"
     }
 }
@@ -30,25 +30,85 @@ if var === 1 return rock
 else if var === 2 return paper
 else if return === 3 scissors 
 else return "Error input"
-*/ 
-function getHumanChoice(){
+*/
+function getHumanChoice() {
     let humanChoice = prompt("Type 1 for rock, 2 for paper, 3 for scissors")
     console.log(humanChoice)
-    if(humanChoice == 1){
+    if (humanChoice == 1) {
         return "Rock"
     }
-    else if(humanChoice == 2){
+    else if (humanChoice == 2) {
         return "Paper"
     }
-    else if(humanChoice == 3){
+    else if (humanChoice == 3) {
         return "Scissors"
     }
-    else{
+    else {
         return "Error input"
     }
 }
 
 /*
 Use the human and computer player choices as arguments
+Repeat the gameplay 5 time
+Every loop check:
+if(humanChoice === computerChoice) 
+    print draw
+    skip to the next game
+else if(humanChoice === "Rock" && computerChoice === "Paper")
+    print you lose
+    add 1 point to computer
+    skip to the next game
+else if(humanChoice === "Rock" && computerChoice === "Scissors")
+    print you win
+    add 1 point to human
+    skip to the next game
+else if(humanChoice === "Paper" && computerChoice === "Scissors")
+    print you lose
+    add 1 point to computer
+    skip to the next game
+else if(humanChoice === "Paper" && computerChoice === "Rock")
+    print you win
+    add 1 point to human
+    skip to the next game
+else if(humanChoice === "Scissors" && computerChoice === "Rock")
+    print you lose
+    add 1 point to computer
+    skip to the next game
+else if(humanChoice === "Scissors" && computerChoice === "Paper")
+    print you win
+    add 1 point to human
+    skip to the next game
 
+End the loop
 */
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Draw")
+    }
+    else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        console.log("You lose")
+        computerScore += 1
+    }
+    else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        console.log("You lose")
+        computerScore += 1
+    }
+    else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+        console.log("You lose")
+        computerScore += 1
+    }
+    else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        console.log("You win")
+        humanScore += 1
+    }
+    else if (humanChoice === "Papper" && computerChoice === "Rock") {
+        console.log("You win")
+        humanScore += 1
+    }
+    else if (humanChoice === "Scissors" && computerChoice === "Papper") {
+        console.log("You win")
+        humanScore += 1
+    }
+}
+
