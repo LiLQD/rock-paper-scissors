5446b3ebf02b87c1d33be83fdeb6713d9c82474f
LiLQD <luuquangdung1511@gmail.com>
2025-09-17 00:58:47 +0700

    Remove duplicate comment on every function

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ let computerScore = 0
 /* 
 Initialize a variable and set it for random value
 Multiple that var with 100 and round to the nearest integer for a value start from 0 to 100
-Use condition statement to check:
-if var <= 33 return rock
-else if var >33 <=66 return paper
-else return scissors 
 */
 function getComputerChoice() {
     let randomNumber = Math.round(Math.random() * 100)
@@ -28,11 +24,6 @@ function getComputerChoice() {
 
 /* 
 Initialize a variable and let it assigned by player by prompt
-Use condition statement to check:
-if var === 1 return rock
-else if var === 2 return paper
-else if return === 3 scissors 
-else return "Error input"
 */
 function getHumanChoice() {
     let humanChoice = prompt("Type 1 for rock, 2 for paper, 3 for scissors")
@@ -55,35 +46,7 @@ function getHumanChoice() {
 }
 
 /*
-Use the human and computer player choices as arguments
-if(humanChoice === computerChoice) 
-    print draw
-    skip to the next game
-else if(humanChoice === "Rock" && computerChoice === "Paper")
-    print you lose
-    add 1 point to computer
-    skip to the next game
-else if(humanChoice === "Rock" && computerChoice === "Scissors")
-    print you win
-    add 1 point to human
-    skip to the next game
-else if(humanChoice === "Paper" && computerChoice === "Scissors")
-    print you lose
-    add 1 point to computer
-    skip to the next game
-else if(humanChoice === "Paper" && computerChoice === "Rock")
-    print you win
-    add 1 point to human
-    skip to the next game
-else if(humanChoice === "Scissors" && computerChoice === "Rock")
-    print you lose
-    add 1 point to computer
-    skip to the next game
-else if(humanChoice === "Scissors" && computerChoice === "Paper")
-    print you win
-    add 1 point to human
-    skip to the next game
-
+Check who is winner by use the human and computer player choices as arguments for condition
 */
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
@@ -115,15 +78,10 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 /*
-Repeat the gameplay 5 time
-Every loop:
-Recall getHumanChoice and assign its to a variable
-Recall getComputerChoice and assign its to a variable
-call playRound function use getHumanChoice and getComputerChoice return value as agruments 
-check who is winner and track score
+Repeat the gameplay 5 time and track score
 */
-function playGame(){
-    for(let i = 0; i < 5; i++){
+function playGame() {
+    for (let i = 0; i < 5; i++) {
         let humanChoice = getHumanChoice()
         let computerChoice = getComputerChoice()
         playRound(humanChoice, computerChoice)
